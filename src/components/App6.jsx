dca97d4c36d71c60088a03cc5d2d3b216cbb6c3a
Sindhuja2535 <sindhuja2516@gamil.com>
2025-05-24 20:18:44 +0530

    app6

---
+++ src/components/App6.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App6.css';
+
+function App6() {
+  return (
+    <div>
+      <h1 className="heading">This is App6 Component</h1>
+      <div className="box-container">
+        <div className="box">Apple</div>
+        <div className="box">Mango</div>
+        <div className="box">Orange</div>
+        <div className="box">Banana</div>
+      </div>
+    </div>
+  );
+}
+
+export default App6;
